5a0c275ad917eeb1e0309087bf3cec57ab67d881
Valerii Udodov <valeriiu@pageuppeople.com>
2019-07-18 16:36:15 +1000

    merge extension

--- V.Udodov.Json/TypeExtensions.cs
+++ V.Udodov.Json/TypeExtensions.cs
@@ -12,7 +12,17 @@ namespace V.Udodov.Json
         /// Generates JSON Schema from the type and merges it with JSON Schema provided in <paramref name="jsonSchema"/>.
         /// Optionally resulting JSON Schema Id might be defined. If not Id will be generated based from the <paramref name="mergeIn"/> full name.  
         /// </summary>
-        public static string Merge(this Type mergeIn, string jsonSchema, Uri id = null)
+        public static string MergeLeft(this Type mergeIn, string jsonSchema, Uri id = null)
+            => Merge(mergeIn, jsonSchema, id, true);
+        
+        /// <summary>
+        /// Generates JSON Schema from the type and merges it into JSON Schema provided in <paramref name="jsonSchema"/>.
+        /// Optionally resulting JSON Schema Id might be defined. If not Id will be generated based from the <paramref name="mergeIn"/> full name.  
+        /// </summary>
+        public static string MergeRight(this Type mergeIn, string jsonSchema, Uri id = null)
+            => Merge(mergeIn, jsonSchema, id, false);
+        
+        private static string Merge(Type mergeIn, string jsonSchema, Uri id = null, bool left = true)
         {
             var schema = new JSchemaGenerator
             {
@@ -22,9 +32,23 @@ namespace V.Udodov.Json
             }.Generate(mergeIn);
 
             if(!string.IsNullOrWhiteSpace(jsonSchema))
-                JSchema.Parse(jsonSchema).Properties.ToList().ForEach(schema.Properties.Add);
+                return left 
+                    ? Merge(schema, JSchema.Parse(jsonSchema)).ToString()
+                    : Merge(JSchema.Parse(jsonSchema), schema).ToString();
 
             return schema.ToString();
+        }
+
+        
+        private static JSchema Merge(JSchema left, JSchema right)
+        {
+            right.Properties.ToList().ForEach(left.Properties.Add);
+            right.ExtensionData.ToList().ForEach(left.ExtensionData.Add);
+            right.Required.ToList().ForEach(left.Required.Add);
+            left.Id = left.Id ?? right.Id;
+            left.SchemaVersion = left.SchemaVersion ?? right.SchemaVersion;
+
+            return left;
         }
     }
 }
